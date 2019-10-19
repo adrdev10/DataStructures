20213fb8fc954284e1e9bb2d60192cd373b46593
adrian <cr7adrian@hotmail.com>
2019-10-19 00:49:43 -0400

    add newstack function

--- linear/stack/main/main.go
+++ linear/stack/main/main.go
@@ -1,1 +1,14 @@
-ECHO is on.
+package main
+
+import (
+	"fmt"
+
+	"github.com/xdragon1015/DataStructures/linear/stack"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	var stk = &stack.Stack{}
+	stk.NewStack()
+	var element1 = &stack.Element{10}
+}
--- linear/stack/stack.go
+++ linear/stack/stack.go
@@ -20,3 +20,26 @@ func (stack *Stack) NewStack() {
 func (element *Element) String() string {
 	return strconv.Itoa(element.elementValue)
 }
+
+func (stack *Stack) Push(element *Element) {
+	stack.elements = append(stack.elements[:stack.elementCount], element)
+	stack.elementCount++
+}
+
+func (stack *Stack) Pop() *Element {
+	if stack.elementCount == 0 {
+		return nil
+	}
+
+	var length = len(stack.elements)
+	var element = stack.elements[length-1]
+
+	if length > 1 {
+		stack.elements = stack.elements[:length-1]
+	} else {
+		stack.elements = stack.elements[0:]
+	}
+	stack.elementCount = len(stack.elements)
+	return element
+
+}
